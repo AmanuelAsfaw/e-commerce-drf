44c317695c851944c18aee0a4c3f732445801b89
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 14:58:46 +0300

    Correct CORS error for api request

--- adey_abeba_backend/settings.py
+++ adey_abeba_backend/settings.py
@@ -40,10 +40,15 @@ INSTALLED_APPS = [
     
     'rest_framework',
 
+    'corsheaders',
+
     'base.apps.BaseConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [    
+    # middleware class for CORS
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
