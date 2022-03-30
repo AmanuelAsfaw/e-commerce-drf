c094ed2e6a2af19ab90f7d1c969c086822924e5c
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 16:52:48 +0300

    Make OrderItem model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from itertools import product
 from operator import mod
 from statistics import mode
@@ -49,4 +50,15 @@ class Order(models.Model):
     def __str__(self) -> str:
         return str(self.createAt)
 
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.createAt)
 
