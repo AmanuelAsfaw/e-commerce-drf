dc96aed1bef9561a857f14cea9743e2f32c91d59
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 12:15:12 +0300

    Add Test Cases

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class BaseTests(APITestCase):
+
+    def test_get_routes(self):
+        """
+        Ensure we can get list of 8 routes.
+        """
+        print('test_get_routes')
+        url = reverse('routes')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 8)
+
+    def test_get_products(self):
+        """
+        Ensure we can get list of 6 products.
+        """
+        print('test_get_products')
+        url = reverse('products')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 6)
+
+    def test_get_product(self):
+        """
+        Ensure we can get list of 6 products.
+        """
+        print('test_get_product')
+        url = reverse('products')
+        print(url+'1')
+        response = self.client.get(url+'1', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Airpods Wireless Bluetooth Headphones')
