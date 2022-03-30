60feef012ce33a542d95bbaee522000ab624f2c9
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 16:57:01 +0300

    Correct corsheader module notfound error

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Order, OrderItem, Product, Review
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
