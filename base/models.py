76bd20bfd1ddd47c79f4f33ea82ea68f71b6719a
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 16:18:56 +0300

    Make Product Model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
+from operator import mod
+from statistics import mode
+from unicodedata import category, name
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    #image
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
