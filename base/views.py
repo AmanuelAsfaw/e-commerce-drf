06914b26d016086b7bf9d130ed209c55a0a2ca4e
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-30 10:28:32 +0300

    add DRF in project

--- base/views.py
+++ base/views.py
@@ -1,11 +1,29 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
-# Create your views here.
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+        
+        '/api/products/upload/',
+        
+        '/api/products/<id>/reviews/',
+        
+        '/api/products/top/',
+        '/api/products/<id>/',
+        
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
 
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
