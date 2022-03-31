0192a633467cddd94a8059ddfbfc8e3dc0dea81a
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-31 08:43:29 +0300

    Make ShippingAddress model

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Order, OrderItem, Product, Review
+from .models import Order, OrderItem, Product, Review, ShippingAddress
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- base/models.py
+++ base/models.py
@@ -1,15 +1,12 @@
-from email.mime import image
-from itertools import product
-from operator import mod
-from statistics import mode
-from unicodedata import category, name
 from django.db import models
+
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    #image
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -57,6 +54,18 @@ class OrderItem(models.Model):
     qty = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.createAt)
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self) -> str:
