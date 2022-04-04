b34ab25dcbcd774ba4589e622a93cfa7f11c5d00
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-04-04 10:32:28 +0300

    Protecte Routes with authorization

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -45,9 +46,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
